9fa01485b386c0325b3b731e31a9e4174b661935
Douglas da Mata <douglasfigsouza@gmail.com>
2017-05-26 11:20:37 -0300

    Version 1.8

--- Gesprog/Scripts/GesprogJs/Controllers.js
+++ Gesprog/Scripts/GesprogJs/Controllers.js
@@ -3,14 +3,30 @@ angular.module("Gesprog")
 .controller("GesprogCtrl", function ($scope, Service) {
     //função que preenche o combo cidades em função do estado
     $scope.getCities = function (id) {
-            Service.GetCidades(id).then(function (response) {
+        Service.GetCidades(id).then(function (response) {
             $scope.cities = response.data;
         });
     }
     //função que cria os checkbox de horarios
     Service.GetHorarios().then(function (response) {
-          $scope.ListHorarios = response.data;
+        $scope.ListHorarios = response.data;
     });
+    $scope.HorariosSelecionados = [];
+    // toggle selection for a given employee by name
+    $scope.toggleSelection = function toggleSelection(employeeName) {
+        var idx = $scope.HorariosSelecionados.indexOf(employeeName);
+        // is currently selected
+        if (idx > -1) {
+            $scope.HorariosSelecionados.splice(idx, 1);
+            console.log($scope.HorariosSelecionados);
+        }
+
+            // is newly selected
+        else {
+            $scope.HorariosSelecionados.push(employeeName);
+            console.log($scope.HorariosSelecionados);
+        }
+    };
 
 })
 .factory('Service', function ($http) {
