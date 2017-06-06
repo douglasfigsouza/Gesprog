cbf8c63eb517f5327a68acdf59823579b4370f1d
Douglas da Mata <douglasfigsouza@gmail.com>
2017-06-06 16:05:50 -0300

    com erro

--- Gesprog/Controllers/ProgramadoresController.cs
+++ Gesprog/Controllers/ProgramadoresController.cs
@@ -13,6 +13,9 @@ namespace Gesprog.Controllers
         HorariosRepository HorariosRep;
         List<HORARIOS> ListaDeHorarios;
         ProgramadoresRepository ProgramadoresRep;
+        BANCO banco;
+        BancosRepository BancosRep;
+        CONTAS_BANCARIAS conta;
 
         public ProgramadoresController()
         {
@@ -20,6 +23,9 @@ namespace Gesprog.Controllers
             this.HorariosRep = new HorariosRepository();
             this.ListaDeHorarios = new List<HORARIOS>();
             this.ProgramadoresRep = new ProgramadoresRepository();
+            this.banco = new BANCO();
+            this.BancosRep = new BancosRepository();
+            this.conta = new CONTAS_BANCARIAS();
         }
         // GET: Programadores
         public ActionResult Add_Programador()
@@ -33,12 +39,34 @@ namespace Gesprog.Controllers
             ModelState.Clear();
             if(ModelState.IsValid)
             {
+                programador.ID_PROG = programador.ID_PROG;
+                programador.CIDADEID = programador.CIDADEID;
+                programador.NOME_PROG = programador.NOME_PROG;
+                programador.CPF_PROG = programador.CPF_PROG;
+                programador.FONE_PROG = programador.FONE_PROG;
+                programador.SKYPE_PROG = programador.SKYPE_PROG;
+                programador.LINKEDIN_PROG = programador.LINKEDIN_PROG;
+                programador.EMAIL_PROG = programador.EMAIL_PROG;
+                programador.PORTIFOLIO_PROG = programador.PORTIFOLIO_PROG;
+                programador.DISPHRTRDIA_PROG = programador.DISPHRTRDIA_PROG;
+                programador.PRETSAL_PROG = programador.PRETSAL_PROG;
+                programador.LINKCRUD_PROG = programador.LINKCRUD_PROG;
+
                 foreach (var item in form["HorariosSelecionados"])
                 {
                     ListaDeHorarios.Add(new HORARIOS {
                         ID_HR = Convert.ToInt32(item)
                     });
                 }
+                banco.NOME_BANCO = form["programador.Banco"];
+                BancosRep.Add_Banco(banco);
+
+                conta.ID_BANCO = banco.ID_BANCO;
+                conta.NUM_CONTA = form["programador.Conta"];
+                conta.AGENCIA_CONTA = form["programador.Agencia"];
+                conta.TIPO_CONTA = form["programador.TipoConta"];
+
+
                 ProgramadoresRep.add(programador, ListaDeHorarios);
 
             }
---
+++ Gesprog/Models/BancosRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gesprog.Models
+{
+    public class BancosRepository
+    {
+        DbGesprog context;
+        public BancosRepository()
+        {
+            this.context = new DbGesprog();
+        }
+        public void Add_Banco(BANCO banco)
+        {
+            try
+            {
+                context.BANCO.Add(banco);
+                context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
