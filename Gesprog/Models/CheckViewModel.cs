5a85c210fa1b51613a4b1465b900b9d6faa5a84b
Douglas da Mata <douglasfigsouza@gmail.com>
2017-06-28 10:37:59 -0300

    Merge branch 'master' of https://github.com/douglasfigsouza/Gesprog.git
    Conflicts:
    	Gesprog/Controllers/ProgramadoresController.cs
    	Gesprog/Models/ProgramadoresRepository.cs
    	Gesprog/Views/Programadores/Add_Programador.cshtml

---
+++ Gesprog/Models/CheckViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gesprog.Models
+{
+    public class CheckViewModel
+    {
+        public int ID { get; set; }
+        public int VALOR { get; set;}
+        }
+}
